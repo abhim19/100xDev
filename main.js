0b9a55963643f46925751917b372104e46ff62fe
Admin <abhimahindrakar19@gmail.com>
2025-01-06 17:52:41 +0530

    my commit

---
+++ main.js
@@ -0,0 +1,67 @@
+// // even numbers in array
+// let a=[12,34,21,5,31,30];
+// for(let i=0; i<a.length; i++) {
+//     if(a[i]%2==0) {
+//         console.log(a[i]);
+//     }
+// }
+
+// // largest in array
+// let a=[12,34,21,5,31,30];
+// let max=a[0];
+// for(let i=0; i<a.length; i++) {
+//     if(a[i]>max) {
+//         max=a[i];
+//     }
+// }
+// console.log(max);
+
+
+// // 
+// function gender() {
+//     const allUsers = [{
+//         firstname: "Abhi",
+//         gender: "Male"
+//     }, {
+//         firstname: "Rahul",
+//         gender: "Male"
+//     }, {
+//         firstname: "Aadya",
+//         gender: "Female"
+//     }]
+
+//     for(let i=0 ;i<allUsers.length; i++) {
+//         if(allUsers[i]["gender"]=="Male") {
+//             console.log(allUsers[i]["firstname"]);
+//         }
+//     }
+// }
+// gender();
+
+
+// // function to find sum 
+// function sum(a,b) {
+//     return a+b;
+// }
+// console.log(sum(10,20));
+
+
+function sum(num1,num2, callback) {
+    let result=num1+num2;
+    callback(result);
+}
+
+function displayResult(data) {
+    console.log("Result of the sum is: "+data);
+}
+
+function displayResultPassive(data) {
+    console.log("Sum's result is: "+data);
+}
+
+
+const ans = sum(2, 4, displayResultPassive)
+
+
+
+
